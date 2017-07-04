ee41b8e6e5bafd210ea0732b168219cb598305f1
Foloba Anca <ursu.andrei1993@gmail.com>
2017-07-04 21:29:12 +0300

    json

--- js/contacte.js
+++ js/contacte.js
@@ -1,8 +1,6 @@
 /**
  * Created by Ursuca on 27.06.2017.
  */
-
-
 function getRow(firstName, lastName, phone)
 {
     if (phone == undefined){
@@ -15,17 +13,6 @@ function getRow(firstName, lastName, phone)
     var row = '<tr> <td>'+ lastName +'</td> <td>' + (firstName || '') +'</td> <td>' + phone +'</td> </tr>'
     return row;
 }
-var contacte=[
-        {lastName:'Ursu', firstName:'Andrei', phone:'0700000000'},
-        {lastName:'Ursu', firstName:'Andrei', phone:'0700000000'},
-        {lastName:'Ursu', firstName:'Andrei', phone:'0700000000'},
-        {lastName:'a', firstName:'b', phone:'0221321'},
-        {lastName:'a', firstName:'b', phone:'0221321'},
-        {lastName:'a', firstName:'b', phone:'0221992321321'},
-        {lastName:'c', firstName:'d', phone:'07874321'},
-        {firstName:'d'},
-        {lastName:'absbas'}
-];
 var person = {
     lastName: "Ursu",
     firstName: "Andrei",
@@ -33,31 +20,31 @@ var person = {
     married: false,
     skills: ["html", "css", "js"],
     kids: [],
-    voiceCall: function(nume)
-{
+    voiceCall: function(nume) {
     console.info("suna pe ", nume);
-}
-,
-partener:
-{
-    firstName:"M",
-        age
-:
-    18
-}
+    },
+    partener: {
+        firstName: "M",
+        age: 18
+    }
 };
 console.info(person.firstName)
 person.voiceCall("Soacra");
 
 
 var tableContent =' ';
-for (var i=0; i<contacte.length; i++) {
-    var contact = contacte[i];
-    tableContent += getRow(contact.firstName, contact.lastName, contact.phone);
-
+// for (var i=0; i<contacte.length; i++) {
+//     createRow(contacte[i]);
+// }
+function createRow(contact) {
+    return tableContent+=getRow(contact.firstName,contact.lastName,contact.phone);
 }
+$.ajax('date/contacte.json').done(function (contacte){
+    console.info('contacte', contacte);
+    contacte.forEach(createRow);
+    $("#contacts-list tbody").html(tableContent);
+    });
 
-$("#contacts-list tbody").html(tableContent);
 
 // 1.convert from array of arrays into json
 // 2. load contacts from json file with AJAX
