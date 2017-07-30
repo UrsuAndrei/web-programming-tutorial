8769585cc065c617c2ac2da14c1ac57a73bcc071
UrsuAndrei <ursu.andrei1993@gmail.com>
2017-07-30 20:44:35 +0300

    add remove

--- js/contacte.js
+++ js/contacte.js
@@ -1,35 +1,35 @@
 /**
  * Created by Ursuca on 27.06.2017.
  */
-function getRow(firstName, lastName, phone)
-{
-    if (phone == undefined){
-        phone='';
-    }
-    if (typeof lastName == 'undefined'){
-        lastName='';
-    }
-    firstName = firstName || '';
-    var row = '<tr> <td>'+ lastName +'</td> <td>' + (firstName || '') +'</td> <td>' + phone +'</td> </tr>'
+function getRow(contact)
+{   var id=contact.id;
+    var firstName = contact.firstName || '';
+    var lastName = contact.lastName || '';
+    var phone = contact.phone || '';
+    var row = '<tr> <td>'+ lastName +'</td> <td>' + (firstName || '') +'</td> <td>' + phone +'</td>'+
+        '<td class="actions">'+'' +
+        '<span><a href="date/remove.html?id=' + id + '"> &#x2718;</a></span>  '+
+        '<span><a href="#">&#x270E;</a></span>'+
+        '</td>' +'</tr>'
     return row;
 }
-var person = {
-    lastName: "Ursu",
-    firstName: "Andrei",
-    age: 23,
-    married: false,
-    skills: ["html", "css", "js"],
-    kids: [],
-    voiceCall: function(nume) {
-    console.info("suna pe ", nume);
-    },
-    partener: {
-        firstName: "M",
-        age: 18
-    }
-};
-console.info(person.firstName)
-person.voiceCall("Soacra");
+// var person = {
+//     lastName: "Ursu",
+//     firstName: "Andrei",
+//     age: 23,
+//     married: false,
+//     skills: ["html", "css", "js"],
+//     kids: [],
+//     voiceCall: function(nume) {
+//     console.info("suna pe ", nume);
+//     },
+//     partener: {
+//         firstName: "M",
+//         age: 18
+//     }
+// };
+// console.info(person.firstName)
+// person.voiceCall("Soacra");
 
 
 var tableContent =' ';
@@ -37,14 +37,13 @@ var tableContent =' ';
 //     createRow(contacte[i]);
 // }
 function createRow(contact) {
-    return tableContent+=getRow(contact.firstName,contact.lastName,contact.phone);
+    return tableContent+=getRow(contact);
 }
 $.ajax('date/contacte.json').done(function (contacte){
     console.info('contacte', contacte);
     contacte.forEach(createRow);
     $("#contacts-list tbody").html(tableContent);
     });
-
 
 // 1.convert from array of arrays into json
 // 2. load contacts from json file with AJAX
